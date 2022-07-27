6315ff72b1b00e55c757c2db1e3e2fb05c8319b0
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-27 07:55:48 -0400

    feat: Replace all instances of var with let

--- src/assets/js/load.js
+++ src/assets/js/load.js
@@ -1,11 +1,11 @@
-var qs = new URLSearchParams(window.location.search);
-var gpg = qs.get('page');
+let qs = new URLSearchParams(window.location.search);
+let gpg = qs.get('page');
 const storage = require('electron-json-storage');
-var { ipcRenderer } = require('electron');
-var fs = require('fs');
+let { ipcRenderer } = require('electron');
+let fs = require('fs');
 this.fs = fs;
 
-var dataPath = "error";
+let dataPath = "error";
 this.dataPath = dataPath;
 
 ipcRenderer.send('json_path');
@@ -36,10 +36,10 @@ function loadPage(pg) {
     }
 
     if (pg === "today" || pg === "upcoming" || pg === "completed") {
-        var dt = new Date();
-        var dy = dt.getDay();
+        let dt = new Date();
+        let dy = dt.getDay();
         if (dy === 0) {dy = "Sunday"} else if (dy === 1) {dy = "Monday"} else if (dy === 2) {dy = "Tuesday"} else if (dy === 3) {dy = "Wednesday"} else if (dy === 4) {dy = "Thursday"} else if (dy === 5) {dy = "Friday"} else if (dy === 6) {dy = "Saturday"} else {dy = "Error"}
-        var fm = dy+", "+("0" + dt.getDate()).slice(-2)+"/"+("0" + (dt.getMonth() + 1)).slice(-2)+"/"+dt.getFullYear();
+        let fm = dy+", "+("0" + dt.getDate()).slice(-2)+"/"+("0" + (dt.getMonth() + 1)).slice(-2)+"/"+dt.getFullYear();
         if (pg === "today"){loadPageTitle(fm);}
 
         loadPosts(dt.getFullYear()+"-"+("0" + (dt.getMonth() + 1)).slice(-2)+"-"+("0" + dt.getDate()).slice(-2));
@@ -58,29 +58,29 @@ async function loadPosts(tc) {
     console.log("loadTodayNotes started")
     const notesfile = await fetch(dataPath+"/posts.json");
     const notesParsed = await notesfile.json();
-    var notes = await notesParsed.table;
+    let notes = await notesParsed.table;
     console.log(notes);
-    var contentsDiv = document.getElementById("page-jsei");
-    var notesLength = Object.keys(notes).length;
+    let contentsDiv = document.getElementById("page-jsei");
+    let notesLength = Object.keys(notes).length;
     console.log(notesLength)
-    var postsLoaded = 0;
-    for (var i = 0; i < notesLength; i++) {
-        var thisNote = notes[i];
-        var thisNoteKeys = Object.keys(thisNote);
+    let postsLoaded = 0;
+    for (let i = 0; i < notesLength; i++) {
+        let thisNote = notes[i];
+        let thisNoteKeys = Object.keys(thisNote);
         console.log("ThisNoteKeys is "+thisNoteKeys)
-        var thisNoteValues = Object.values(thisNote);
+        let thisNoteValues = Object.values(thisNote);
         console.log("ThisNoteValues is "+thisNoteValues)
 
         console.log("for loop iteration "+i)
         console.log("date: "+thisNoteValues[0]+" needs to be equal to TC:"+tc+" or endless")
 
-        var upcomingBoolean = false;
+        let upcomingBoolean = false;
         if (gpg === "upcoming") {
             if (tc === "endless") {
                 upcomingBoolean = true;
             }
             else {
-                var mba = new Date(tc);
+                let mba = new Date(tc);
                 console.log("mba/today ("+mba.getFullYear()+"-"+("0" + (mba.getMonth() + 1)).slice(-2)+"-"+("0" + mba.getDate()).slice(-2)+") must be before or equal to post's date ("+thisNoteValues[0]+") - RESULT: "+(mba <= new Date(thisNoteValues[0])));
                 upcomingBoolean = mba <= new Date(thisNoteValues[0]);
             }
@@ -90,13 +90,13 @@ async function loadPosts(tc) {
         if ((gpg === "today" && thisNoteValues[4] === "visible") || (gpg === "completed" && thisNoteValues[4] === "completed") || (gpg === "upcoming" && thisNoteValues[4] === "visible")) {
             if (gpg==="completed" || thisNoteValues[0] === tc || thisNoteValues[0] === "endless" || upcomingBoolean) {
                 console.log("found post for today")
-                var unCompleteIcon = "fa-check";
-                var unComplete = "Complete";
+                let unCompleteIcon = "fa-check";
+                let unComplete = "Complete";
                 if (gpg === "completed") {unComplete = "Uncomplete"; unCompleteIcon = "fa-angle-left";}
 
                 if (thisNoteValues[1] === "todo") {
-                    var contentPart = "";
-                    for (var it=0;it<thisNoteValues[3].length; it++) {
+                    let contentPart = "";
+                    for (let it=0;it<thisNoteValues[3].length; it++) {
                         console.log(Object.values(thisNoteValues[3])[it].val);
                         if (Object.values(thisNoteValues[3])[it].val === true) {
                             contentPart += 
@@ -153,7 +153,7 @@ this.loadPosts = loadPosts;
 
 function loadComplete() {
     if (!fs.existsSync(dataPath+"/posts.json")) {
-        var data2Write = {table: [{"date": "endless","type": "note","title": "Welcome to Foxlmind...","content": "Thank you for using Foxlmind!<br>This was made completely for fun but if you want to support its development, you can <a onclick='openURLInBrowser(`https://patreon.com/foxlldev`)' href='#'>donate here</a>.<br>If not, that's fine, enjoy :)","state": "visible"},{"date": "endless","type": "note","title": "Tribute to Technoblade","content": "https://youtu.be/DPMluEVUqS0","state": "visible"}]};
+        let data2Write = {table: [{"date": "endless","type": "note","title": "Welcome to Foxlmind...","content": "Thank you for using Foxlmind!<br>This was made completely for fun but if you want to support its development, you can <a onclick='openURLInBrowser(`https://patreon.com/foxlldev`)' href='#'>donate here</a>.<br>If not, that's fine, enjoy :)","state": "visible"},{"date": "endless","type": "note","title": "Tribute to Technoblade","content": "https://youtu.be/DPMluEVUqS0","state": "visible"}]};
         fs.writeFile(dataPath+"/posts.json", JSON.stringify(data2Write, null, 2), { flag: 'wx' }, function (err) {
             if (err) throw err;
             console.log("It's saved!");
