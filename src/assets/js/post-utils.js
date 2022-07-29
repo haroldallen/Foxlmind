6b76b1ec358d6a70b1e889a36950a3806dfadf22
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-29 08:28:23 -0400

    chore: Update all vars in post-utils into lets

--- src/assets/js/post-utils.js
+++ src/assets/js/post-utils.js
@@ -2,7 +2,7 @@ function writeToNotes(jsonData) {
     fs.readFile(dataPath+"/posts.json", 'utf8', function readFileCallback(err, data) {
         if (err) {throw err;}
         console.log("Data served");
-        var obj = JSON.parse(data);
+        let obj = JSON.parse(data);
         obj.table.push(jsonData);
         fs.writeFile(dataPath+"/posts.json", JSON.stringify(obj, null, 2), 'utf8', (err) => {if(err) {throw(err);}});
     });
@@ -13,7 +13,7 @@ function updateValueInPost(id, key, value) {
     fs.readFile(dataPath+"/posts.json", 'utf8', function readFileCallback(err, data) {
         if (err) {throw err;}
         console.log("Data served");
-        var obj = JSON.parse(data);
+        let obj = JSON.parse(data);
         Object.values(obj.table)[id][key] = value;
         fs.writeFile(dataPath+"/posts.json", JSON.stringify(obj, null, 2), 'utf8', (err) => {if(err) {throw(err);}});
     });
@@ -24,7 +24,7 @@ function updateTodoValueInPost(pid, cid, key, value) {
     fs.readFile(dataPath+"/posts.json", 'utf8', function readFileCallback(err, data) {
         if (err) {throw err;}
         console.log("Data served");
-        var obj = JSON.parse(data);
+        let obj = JSON.parse(data);
         console.log(Object.values(obj.table));
         console.log(Object.values(obj.table)[pid]);
         console.log(Object.values(Object.values(obj.table)[pid]));
@@ -42,9 +42,9 @@ function deletePost(id) {
     fs.readFile(dataPath+"/posts.json", 'utf8', function readFileCallback(err, data) {
         if (err) {throw err;}
         console.log("Data served");
-        var objS = JSON.parse(data).table;
+        let objS = JSON.parse(data).table;
         objS.splice(id, 1);
-        var obj = JSON.parse(data);
+        let obj = JSON.parse(data);
         obj.table = objS;
         
         fs.writeFile(dataPath+"/posts.json", JSON.stringify(obj, null, 2), 'utf8', (err) => {if(err) {throw(err);}});
