3b30ed837f1e90247142bb75579e4684fa918d11
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-30 10:39:22 -0400

    chore: Add another comment to avoid any confusion

--- src/assets/js/utility/verbose.js
+++ src/assets/js/utility/verbose.js
@@ -1,4 +1,4 @@
-const isVerbose = true;
+const isVerbose = true; // Using this each module importing it globally will be verbose or not verbose
 
 /**
  * Alternative to console.log since one of the people who reviewed the code complained about console.log being left everywhere.
