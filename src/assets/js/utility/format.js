93b693ccbc55eb3abb8d2326411d7557d09cac33
sorcerer.worker <sorcerer.worker@yandex.com>
2022-07-30 10:29:26 -0400

    feat: Add more comments for documentation

--- src/assets/js/utility/format.js
+++ src/assets/js/utility/format.js
@@ -1,7 +1,13 @@
+/**
+ * Formats a day string to match whatever format was already hard coded in.
+ */
 function formatDay(day, dateInstance) {
     return day + ", " + ("0" + dateInstance.getDate()).slice(-2) + "/" + ("0" + (dateInstance.getMonth() + 1)).slice(-2) + "/" + dateInstance.getFullYear()
 }
 
+/**
+ * Formats a date instance to match whatever format was already hard coded in.
+ */
 function formatDate(dateInstance) {
     return dateInstance.getFullYear()+"-"+("0" + (dateInstance.getMonth() + 1)).slice(-2)+"-"+("0" + dateInstance.getDate()).slice(-2)
 }
--- src/assets/js/utility/verbose.js
+++ src/assets/js/utility/verbose.js
@@ -1,5 +1,8 @@
 const isVerbose = true;
 
+/**
+ * Alternative to console.log since one of the people who reviewed the code complained about console.log being left everywhere.
+ */
 function verbose(...args) {
     if (!isVerbose) {return}
     console.log(...args)
